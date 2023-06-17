1caea3268c2c235bfd0019d0319caf2c1aff0fba
tanminh2003 <21130171@st.hcmuaf.edu.vn>
2023-06-17 15:23:48 +0700

    first update

---
+++ Paint/src/main/java/view/MyFrame.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    private MyPanel panel;
+
+    public MyFrame() {
+        panel = new MyPanel();
+        add(panel);
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+}
