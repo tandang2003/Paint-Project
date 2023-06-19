c70f5ab6e23443632d24dd3e8d11735d0fc4c1a5
bangg2309 <tuyeu1aido@gmail.com>
2023-06-19 14:01:40 +0700

    Test

--- Paint/src/main/java/view/MyPanel.java
+++ Paint/src/main/java/view/MyPanel.java
@@ -6,6 +6,6 @@ import java.awt.*;
 public class MyPanel extends JPanel {
     MyPanel(){
         this.setBounds(0,0,500,400);
-        this.setBackground(Color.CYAN);
+        this.setBackground(Color.YELLOW);
     }
 }
