0d45523f1ccd313d3e77c00282918fe0fd7ad41b
MinhChien <21130010@st.hcmuaf.edu.vn>
2023-06-21 13:17:06 +0700

    fine

--- Paint/src/main/java/view/MyPanel.java
+++ Paint/src/main/java/view/MyPanel.java
@@ -12,10 +12,7 @@ public class MyPanel extends JPanel {
         this.setBounds(0,     0, 500, 400);
         this.setBackground(Color.CYAN);
         this.add(new ColorPanel(controller), BorderLayout.NORTH);
-        this.add(new ColorPanel(controller));
-
         this.add(new ShapPanel(controller), BorderLayout.WEST);
         this.add(new DrawPaint(controller), BorderLayout.CENTER);
-        this.add(new DrawPaint(controller),BorderLayout.CENTER);
     }
 }
