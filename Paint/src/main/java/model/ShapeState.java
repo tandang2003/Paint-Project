afb95891652130fe6bdbe72d4b08a78f7099ad49
Thinhcongtu121103 <thinhcongtu121103@gmail.com>
2023-06-21 09:05:18 +0700

    Add new file Line, Rho

--- Paint/src/main/java/model/ShapeState.java
+++ Paint/src/main/java/model/ShapeState.java
@@ -1,2 +1,41 @@
-package model;public class ShapeState {
+package model;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+
+
+public class ShapeState {
+	public static AShape currShape = new Line();
+	public static int typeShape = 1;
+	public static Color currColor = Color.black;
+	public static ArrayList<AShape> listShape = new ArrayList<>();
+//	public static ArrayList<Color> listColor = new ArrayList<>();
+
+	public static void add() {
+		currShape.setColor(currColor);
+		listShape.add(currShape);
+		currShape = getShape();
+	}
+
+	public static void createShape() {
+		currShape = getShape();
+		currShape.setColor(currColor);
+	}
+
+	public static void setCurrColor(Color color) {
+		currColor = color;
+	}
+
+	private static AShape getShape() {
+		switch (typeShape) {
+		case 0:
+			return new Rhombus();
+		case 1:
+			return new Line();
+		
+		
+		}
+		return currShape;
+	}
 }
--- Paint/src/main/java/view/MyFrame.java
+++ Paint/src/main/java/view/MyFrame.java
@@ -17,7 +17,7 @@ public class MyFrame extends JFrame {
 
         // vu xuan chien test
         setVisible(true);
-=======
+
 //        setVisible(true);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
